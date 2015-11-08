19d6ec747e92922c6b6d1e5bdb87e1e02ac7e82a
kelvin chandra <kelvinchandra1024@gmail.com>
2015-11-08 15:22:00 +0700

    test source commit

---
+++ LodeRunner/GLUTMain.cpp
@@ -0,0 +1,91 @@
+#include "Globals.h"
+#include "cGame.h"
+
+//Delete console
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+
+cGame Game;
+
+void AppReshape(int w, int h)
+{
+	Game.Reshape(w, h);
+}
+void AppRender()
+{
+	Game.Render();
+}
+void AppKeyboard(unsigned char key, int x, int y)
+{
+	Game.ReadKeyboard(key, x, y, true);
+}
+void AppKeyboardUp(unsigned char key, int x, int y)
+{
+	Game.ReadKeyboard(key, x, y, false);
+}
+void AppSpecialKeys(int key, int x, int y)
+{
+	Game.ReadSpecialKeyboard(key, x, y, true);
+}
+void AppSpecialKeysUp(int key, int x, int y)
+{
+	Game.ReadSpecialKeyboard(key, x, y, false);
+}
+void AppMouse(int button, int state, int x, int y)
+{
+	Game.ReadMouse(button, state, x, y);
+}
+void AppIdle()
+{
+	if (!Game.Loop()) exit(0);
+}
+
+void main(int argc, char** argv)
+{
+	int res_x, res_y, pos_x, pos_y;
+
+	//GLUT initialization
+	glutInit(&argc, argv);
+
+	//RGBA with double buffer
+	glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE | GLUT_DEPTH);
+
+	//Create centered window
+	res_x = glutGet(GLUT_SCREEN_WIDTH);
+	res_y = glutGet(GLUT_SCREEN_HEIGHT);
+	pos_x = (res_x >> 1) - (GAME_WIDTH >> 1);
+	pos_y = (res_y >> 1) - (GAME_HEIGHT >> 1);
+
+	glutInitWindowPosition(pos_x, pos_y);
+	glutInitWindowSize(GAME_WIDTH, GAME_HEIGHT);
+	glutCreateWindow("2DGame");
+
+	/*glutGameModeString("800x600:32");
+	glutEnterGameMode();*/
+
+	//Make the default cursor disappear
+	//glutSetCursor(GLUT_CURSOR_NONE);
+
+	//Register callback functions
+	glutReshapeFunc(AppReshape);
+	glutDisplayFunc(AppRender);
+	glutKeyboardFunc(AppKeyboard);
+	glutKeyboardUpFunc(AppKeyboardUp);
+	glutSpecialFunc(AppSpecialKeys);
+	glutSpecialUpFunc(AppSpecialKeysUp);
+	glutMouseFunc(AppMouse);
+	glutIdleFunc(AppIdle);
+
+	//GLEW initialization
+	GLint GlewInitResult = glewInit();
+	if (GLEW_OK != GlewInitResult)
+	{
+		printf("ERROR: %s\n", glewGetErrorString(GlewInitResult));
+		exit(EXIT_FAILURE);
+	}
+
+	//Game initializations
+	Game.Init(1);
+
+	//Application loop
+	glutMainLoop();
+}
---
+++ LodeRunner/Globals.h
@@ -0,0 +1,11 @@
+#pragma comment(lib,"glew32")
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <list>
+#include <fstream>
+#include <string>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
